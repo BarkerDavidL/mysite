3961a57bef6be0f435af39b67c3e028621c353a5
David Barker <dave@ncomtech.com>
2017-11-05 13:17:48 -0500

    added model for blog, admin URL, blog html

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
+from django.urls import resolve
+from django.http import HttpRequest
 
-# Create your tests here.
+from blog.views import home_page
+from blog.models import Post
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_returms_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>Blog</title>', html)
+        self.assertTrue(html.strip().endswith('</html>'))
+
+class PostModelTest(TestCase):
+
+    def test_saving_and_retrieving_posts(self):
+        first_post = Post()
+        first_post.title = 'The first post'
+        first_post.text = 'first blog text'
+        first_post.save()
+
+        second_post = Post()
+        second_post.title = 'The second post'
+        second_post.text = 'second blog text'
+        second_post.save()
+
+        saved_posts = Post.objects.all()
+        self.assertEqual(saved_posts.count(), 2)
+
+        first_saved_post = saved_posts[0]
+        second_saved_post = saved_posts[1]
+        self.assertEqual(first_saved_post.title, 'The first post')
+        self.assertEqual(first_saved_post.text, 'first blog text')
+        self.assertEqual(second_saved_post.title, 'The second post')
+        self.assertEqual(second_saved_post.text, 'second blog text')
+
--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,26 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+
+    def test_can_view_blog(self):
+
+        # Dave wants to view a blog site
+        self.browser.get('http://localhost:8000')
+
+        # He sees the blog page and a header about Blogs
+        assert 'Blog' in self.browser.title
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Blog', header_text)
+
+
+if __name__ in '__main__':
+    unittest.main()
+
